9aa8d36bf118f45bb33290191d061ab7aeb23fcb
Kauafelippe <kauabasiliocunha@gmail.com>
2024-10-08 09:32:59 -0300

    k

--- 99/alas.php
+++ 99/alas.php
@@ -52,7 +52,7 @@
                                     style="color: rgba(255, 255, 255, 0.8);">CURSOS</span></strong></a></li>
                     <li class="nav-item"><a class="nav-link" href="edital.php" style="padding-top: 0px;"><strong><span
                                     style="color: rgba(255, 255, 255, 0.8);">EDITAL</span></strong></a></li>
-                    <li class="nav-item"><a class="nav-link" href="eventos.php"
+                    <li class="nav-item"><a class="nav-link" href="ediatalParte1.php"
                             style="margin-bottom: -22px;padding-top: 0px;padding-bottom: 0px;"><strong><span
                                     style="color: rgba(255, 255, 255, 0.8);">EVENTOS</span></strong></a></li>
                     <li class="nav-item"><a class="nav-link" href="alas.php"><strong><span
